07de21e5bad78973bfea21bce50723a5ad82098e
lcht2701 <lecanhhoangthanh@gmail.com>
2023-03-14 23:58:56 +0700

    feat: fix navigation drawer (1/3)

---
+++ app/src/main/java/com/example/clothesshop/ui/profile/ProfileFragment.java
@@ -0,0 +1,27 @@
+package com.example.clothesshop.ui.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.clothesshop.databinding.FragmentProfileBinding;
+
+public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+}
