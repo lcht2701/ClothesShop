f5dc74a2cd7f052d558c5c34e4dc39400bce1898
Duy Le <duyle210101@gmail.com>
2023-03-14 14:41:30 +0700

    config to JDK17 and version 7.3.1 + add login and regis using firebase

--- app/src/main/java/com/example/clothesshop/LoginActivity.java
+++ app/src/main/java/com/example/clothesshop/LoginActivity.java
@@ -1,23 +1,34 @@
 package com.example.clothesshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     Button signIn;
     EditText email, password;
     TextView signUp;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
 
         signIn = findViewById(R.id.login_btn);
         email = findViewById(R.id.email_login);
@@ -34,7 +45,38 @@ public class LoginActivity extends AppCompatActivity {
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                loginUser();
 
+            }
+        });
+    }
+
+    private void loginUser() {
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
+        if(TextUtils.isEmpty(userEmail)) {
+            Toast.makeText(this,"Mail is empty!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(userPassword)) {
+            Toast.makeText(this,"Password is empty!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(userPassword.length() < 6) {
+            Toast.makeText(this,"Password length must be greater than 6 letters!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //Login User
+        auth.signInWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "Login Successfully!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, "Error" + task.getException(), Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
--- app/src/main/java/com/example/clothesshop/RegistrationActivity.java
+++ app/src/main/java/com/example/clothesshop/RegistrationActivity.java
@@ -10,17 +10,32 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.clothesshop.models.UserModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     Button signUp;
     EditText name, email, password;
     TextView signIn;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
 
         signUp = findViewById(R.id.register_btn);
         name = findViewById(R.id.name_register);
@@ -35,11 +50,50 @@ public class RegistrationActivity extends AppCompatActivity {
             }
         });
 
-//        signUp.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                createUser();
-//            }
-//        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUser();
+            }
+        });
+    }
+    private void createUser() {
+        String userName = name.getText().toString();
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
+        if(TextUtils.isEmpty(userName)) {
+            Toast.makeText(this,"Name is empty!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(userEmail)) {
+            Toast.makeText(this,"Mail is empty!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(userPassword)) {
+            Toast.makeText(this,"Password is empty!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(userPassword.length() < 6) {
+            Toast.makeText(this,"Password length must be greater than 6 letters!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //Create User
+        auth.createUserWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+
+                    UserModel userModel = new UserModel(userName,userEmail,userPassword);
+                    String id = Objects.requireNonNull(task.getResult().getUser()).getUid();
+                    database.getReference().child("Users").child(id).setValue(userModel);
+
+                    Toast.makeText(RegistrationActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(RegistrationActivity.this, "Error" + task.getException(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
