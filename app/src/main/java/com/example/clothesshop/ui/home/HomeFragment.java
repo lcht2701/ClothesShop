fb065ab94fc6c57ec78ff88cb610f2dddbecccf1
lcht2701 <lecanhhoangthanh@gmail.com>
2023-03-15 20:45:18 +0700

    feat: add recycleview popularRec (not done)

--- app/src/main/java/com/example/clothesshop/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/clothesshop/ui/home/HomeFragment.java
@@ -9,18 +9,28 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.clothesshop.R;
+import com.example.clothesshop.adapters.PopularAdapter;
 import com.example.clothesshop.databinding.FragmentHomeBinding;
+import com.example.clothesshop.models.PopularModel;
+
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    RecyclerView popularRec;
     private FragmentHomeBinding binding;
+    //Popular items
+    List<PopularModel> popularModelList;
+    PopularAdapter popularAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        popularRec = root.findViewById(R.id.popular_rec);
+        return root;
     }
 
 //    @Override
