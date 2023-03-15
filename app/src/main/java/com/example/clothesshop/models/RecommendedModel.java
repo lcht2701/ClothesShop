b0abfba4294e17137b8d72136a8e21da5ddb8261
lcht2701 <lecanhhoangthanh@gmail.com>
2023-03-15 23:18:20 +0700

    feat: finish HomeFragment

--- app/src/main/java/com/example/clothesshop/models/RecommendedModel.java
+++ app/src/main/java/com/example/clothesshop/models/RecommendedModel.java
@@ -1,4 +1,60 @@
 package com.example.clothesshop.models;
 
 public class RecommendedModel {
+    String name;
+    String img_url;
+    String description;
+    int price;
+    String rating;
+
+    public RecommendedModel(String name, String img_url, String description, int price, String rating) {
+        this.name = name;
+        this.img_url = img_url;
+        this.description = description;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public RecommendedModel() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImg_url() {
+        return img_url;
+    }
+
+    public void setImg_url(String img_url) {
+        this.img_url = img_url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
 }
--- app/src/main/java/com/example/clothesshop/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/clothesshop/ui/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,9 +16,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.clothesshop.R;
 import com.example.clothesshop.adapters.HomeCategoryAdapter;
 import com.example.clothesshop.adapters.HomePopularAdapter;
+import com.example.clothesshop.adapters.RecommendedAdapter;
 import com.example.clothesshop.databinding.FragmentHomeBinding;
 import com.example.clothesshop.models.CategoryModel;
 import com.example.clothesshop.models.PopularModel;
+import com.example.clothesshop.models.RecommendedModel;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -28,15 +32,20 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    RecyclerView popularRec, homeCategoryRec;
+    RecyclerView popularRec, homeCategoryRec, recommendedRec;
     FirebaseFirestore db;
     private FragmentHomeBinding binding;
+    ProgressBar progressBar;
+    ScrollView scrollView;
     //Popular items
     List<PopularModel> popularModelList;
     HomePopularAdapter popularAdapter;
     //Home Categories
     List<CategoryModel> categoryModelList;
     HomeCategoryAdapter homeCategoryAdapter;
+    //Recommended
+    List<RecommendedModel> recommendedModelList;
+    RecommendedAdapter recommendedAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -44,6 +53,13 @@ public class HomeFragment extends Fragment {
 
         popularRec = root.findViewById(R.id.popular_rec);
         homeCategoryRec = root.findViewById(R.id.explore_rec);
+        recommendedRec = root.findViewById(R.id.recommended_rec);
+        progressBar = root.findViewById(R.id.progress_bar);
+        scrollView = root.findViewById(R.id.scroll_view);
+
+        progressBar.setVisibility(View.VISIBLE);
+        scrollView.setVisibility(View.GONE);
+
         db = FirebaseFirestore.getInstance();
         //Popular Items
         popularRec.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
@@ -62,6 +78,8 @@ public class HomeFragment extends Fragment {
                                 PopularModel popularModel = document.toObject(PopularModel.class);
                                 popularModelList.add(popularModel);
                                 popularAdapter.notifyDataSetChanged();
+                                progressBar.setVisibility(View.GONE);
+                                scrollView.setVisibility(View.VISIBLE);
                             }
                         } else {
                             Toast.makeText(getActivity(), "Error" + task.getException(), Toast.LENGTH_SHORT).show();
@@ -80,11 +98,34 @@ public class HomeFragment extends Fragment {
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if(task.isSuccessful()) {
+                        if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
                                 CategoryModel homeCategoryModel = documentSnapshot.toObject(CategoryModel.class);
                                 categoryModelList.add(homeCategoryModel);
                                 homeCategoryAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error" + task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        //Recommended
+        recommendedRec.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+        recommendedModelList = new ArrayList<>();
+        recommendedAdapter = new RecommendedAdapter(getActivity(), recommendedModelList);
+        recommendedRec.setAdapter(recommendedAdapter);
+
+        db.collection("Recommended")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                                RecommendedModel recommendedModel = documentSnapshot.toObject(RecommendedModel.class);
+                                recommendedModelList.add(recommendedModel);
+                                recommendedAdapter.notifyDataSetChanged();
                             }
                         } else {
                             Toast.makeText(getActivity(), "Error" + task.getException(), Toast.LENGTH_SHORT).show();
